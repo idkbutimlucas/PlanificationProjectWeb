0507c47178a67ea2293ca931435d7703283e6ece
Lilian <58983575+Lilian-DL@users.noreply.github.com>
2022-01-09 13:52:34 +0100

    change

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,29 +2,30 @@
 
 namespace App\Controller;
 
-use Google\Cloud\Firestore\FirestoreClient;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Kreait\Firebase\Firestore;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Kreait\Firebase\Firestore;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 class HomeController extends AbstractController
 {
-    /*private Firestore $database;
-
+    private Firestore $firestore;
     public function __construct(Firestore $firestore)
     {
-        $this->database = $firestore;
-    }*/
+        $this->firestore = $firestore;
+    }
 
     #[Route('/home', name: 'home')]
     public function index(): Response
     {
-        $firestore = new FirestoreClient();
-        $collectionReference = $firestore->collection('User');
+        $database = $this->firestore->database();
+        
+        $collectionReference = $database->collection('User');
         $documentReference = $collectionReference->document('F4UNj1km4ZXnZEEczinM');
         $snapshot = $documentReference->snapshot();
 
+        dd($snapshot);
 
         return $this->render('home/index.html.twig', [
             'FirstName' => $snapshot['FirstName'],
